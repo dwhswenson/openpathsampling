4a4f0ae9e5ff8f264873321c5f545d74c03df629
David W.H. Swenson <dwhs@hyperblazer.net>
2018-07-21 19:09:32 +0200

    Update ensemble_slots for RETIS

--- openpathsampling/experimental/dask/ensemble_slots.py
+++ openpathsampling/experimental/dask/ensemble_slots.py
@@ -1,5 +1,66 @@
 import random
 from openpathsampling.netcdfplus import StorableNamedObject
+import numpy as np
+
+from .serialize_wrapping import serialize_wrap, unwrap
+from openpathsampling.experimental.storage.serialization_helpers import \
+        get_uuid
+
+def select_movers(scheme, n_steps):
+    """Select movers from a scheme (skipping RandomChoice of move tree)
+
+    Parameters
+    ----------
+    scheme : :class:`.MoveScheme`
+        move scheme; must be one that has ``choice_probability`` or
+        ``real_schoice_probability`` set
+    n_step : int
+        number of trial moves to generate
+
+    Returns
+    -------
+    list of :class:`.PathMover`
+        moves to perform, in order
+    """
+    movers, probs = zip(*list(scheme.real_choice_probability.items()))
+    mover_uuids = {get_uuid(m): m for m in movers}
+    mover_uuid_array = np.array([get_uuid(m) for m in movers])
+    preselected_uuids = [np.random.choice(mover_uuid_array, p=probs)
+                         for i in range(n_steps)]
+    preselected_movers = [mover_uuids[u] for u in preselected_uuids]
+    return preselected_movers
+
+def serialized_ensemble_slots_from_sample_set(sample_set):
+    ensemble_slots = ensemble_slots_from_sample_set(sample_set)
+    return {get_uuid(ens): serialize_wrap(slot)
+            for (ens, slot) in ensemble_slots.items()}
+
+def select_samples_from_slots(s_slot_list):
+    slots = [unwrap(s_slot) for s_slot in s_slot_list]
+    samples = [slot.choose() for slot in slots]
+    return serialize_wrap(samples)
+
+def move_task(s_mover, s_samples):
+    mover = unwrap(s_mover)
+    samples = unwrap(s_samples)
+    change = mover.move_core(samples)
+    return serialize_wrap(change)
+
+def update_slots(s_change, s_input_samples, s_slot):
+    change = unwrap(s_change)
+    input_samples = unwrap(s_input_samples)
+    slot = unwrap(s_slot)
+    slot.update(input_samples, change)
+    return serialize_wrap(slot)
+
+def unwrap_serialized_ensemble_slots(s_ensemble_slots):
+    ensemble_slots = {}
+    for ens_uuid, s_slot in s_ensemble_slots.items():
+        slot = unwrap(s_slot)
+        if get_uuid(slot.ensemble) != ens_uuid:
+            raise RuntimeError("Unexpected ensemble")
+        ensemble_slots[slot.ensemble] = slot
+    return ensemble_slots
 
 def ensemble_slots_from_sample_set(sample_set):
     """Create dict of ensemble slot objects from a sample set
