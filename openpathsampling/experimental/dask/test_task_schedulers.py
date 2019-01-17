18cea4877b0efb5fe9f1daf1b8ada294f16b2624
David W.H. Swenson <dwhs@hyperblazer.net>
2019-01-17 15:47:52 +0100

    Update tests for pytest

--- openpathsampling/experimental/dask/test_task_schedulers.py
+++ openpathsampling/experimental/dask/test_task_schedulers.py
@@ -1,7 +1,8 @@
 from __future__ import absolute_import
-from nose.tools import (assert_equal, assert_not_equal, assert_almost_equal,
-                        raises, assert_true)
-from nose.plugins.skip import Skip, SkipTest
+import pytest
+# from nose.tools import (assert_equal, assert_not_equal, assert_almost_equal,
+                        # raises, assert_true)
+# from nose.plugins.skip import Skip, SkipTest
 
 from openpathsampling.tests.test_helpers import (
     true_func, assert_equal_array_array, make_1d_traj, data_filename,
@@ -10,7 +11,7 @@ from openpathsampling.tests.test_helpers import (
 
 import openpathsampling as paths
 
-from openpathsampling.task_schedulers import *
+from openpathsampling.experimental.dask.task_schedulers import *
 
 import logging
 logging.getLogger('openpathsampling.initialization').setLevel(logging.CRITICAL)
@@ -33,22 +34,43 @@ class TestTaskScheduler(object):
         self.sched = TaskScheduler()
 
     def test_wrap_task(self):
-        assert_equal(self.sched.wrap_task(mock_task, 1, kwargument=2), 3)
+        assert self.sched.wrap_task(mock_task, 1, kwargument=2) == 3
 
     def test_wrap_hook(self):
-        assert_equal(self.sched.wrap_hook(mock_task, 1, kwargument=2), 3)
+        assert self.sched.wrap_hook(mock_task, 1, kwargument=2) == 3
+
 
 class TestDaskTaskScheduler(object):
+    @staticmethod
+    def setup_test_cluster(distributed, n_workers=4, n_attempts=3):
+        """Set up a test cluster using dask.distributed. Try up to
+        n_attempts times, and skip the test if all attempts fail."""
+        # taken from contact map
+        cluster = None
+        for _ in range(n_attempts):
+            try:
+                cluster = distributed.LocalCluster(n_workers=n_workers)
+            except AttributeError:
+                # should never get here, because should have already skipped
+                pytest.skip("dask.distributed not installed")
+            except distributed.TimeoutError:
+                continue
+            else:
+                return cluster
+        # only get here if all retries fail
+        pytest.skip("Failed to set up distributed LocalCluster")
+
     def setup(self):
-        if not HAS_DASK:  # imported in the *-import of task_scheduler
-            raise SkipTest
-        client = dask.distributed.Client()
+        dask = pytest.importorskip('dask')  # pylint: disable=W0612
+        distributed = pytest.importorskip('dask.distributed')
+        cluster = self.setup_test_cluster(distributed, n_workers=4)
+        client = distributed.Client(cluster)
         self.sched = DaskTaskScheduler(client)
 
     def test_wrap_task(self):
         wrapped = self.sched.wrap_task(mock_task, 1, kwargument=2)
-        assert_true(isinstance(wrapped, dask.distributed.Future))
-        assert_equal(wrapped.result(), 3)
+        assert isinstance(wrapped, dask.distributed.Future)
+        assert wrapped.result() == 3
 
     def test_wrap_hook(self):
         # TODO: modify hooks to allow stored state as output?
